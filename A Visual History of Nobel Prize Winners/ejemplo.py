e25f9da741b8b9efc1e83fe0e02f6ee8dd3adfab
David Guzzi <dguzzi_10@hotmail.com>
2024-08-22 14:30:04 -0300

    Update ejemplo.py

--- A Visual History of Nobel Prize Winners/ejemplo.py
+++ A Visual History of Nobel Prize Winners/ejemplo.py
@@ -1,1 +1,14 @@
-#Veremos si los cambios impactan en Github.
+#%%
+import pandas as pd
+import numpy as np
+
+np.random.seed(1)
+
+data = {'A':['a','b','c','d','e','f','g','h','i','j'],
+        'B':np.random.exponential(0.5, 10)}
+df = pd.DataFrame(data)
+df
+import matplotlib.pyplot as plt
+
+plt.hist(df['B'])
+plt.show()
